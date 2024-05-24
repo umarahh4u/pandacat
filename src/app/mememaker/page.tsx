62c7ec8590557dce2dd8a5828b567697590ac812
umarahh4u <umarahh4u@gmail.com>
2024-05-24 13:13:46 +0100

    meme select template implemented

--- src/app/mememaker/page.tsx
+++ src/app/mememaker/page.tsx
@@ -1,18 +1,87 @@
 "use client";
 
 import React, { useState, useEffect, useCallback } from "react";
-import { chakra, Box, Text, Divider } from "@chakra-ui/react";
+import { chakra, Box, Text, Divider, SimpleGrid, Flex } from "@chakra-ui/react";
 import { useDropzone } from "react-dropzone";
 import { CiFileOn } from "react-icons/ci";
+import { IoIosCheckbox } from "react-icons/io";
 
 import { navMenus } from "@/constants/navMenuConstants";
 import NavBar from "@/components/home/NavBar";
 import CustomImage from "@/components/Image";
+import PandaCat from "@/constants/colorConstants";
+import CustomButton from "@/components/Button";
+
+const imageList = [
+  {
+    id: 1,
+    url: "/img/herobg.webp",
+    checked: false,
+  },
+  {
+    id: 2,
+    url: "/img/herobg.webp",
+    checked: false,
+  },
+  {
+    id: 3,
+    url: "/img/herobg.webp",
+    checked: false,
+  },
+  {
+    id: 4,
+    url: "",
+    checked: false,
+  },
+  {
+    id: 5,
+    url: "",
+    checked: false,
+  },
+  {
+    id: 6,
+    url: "",
+    checked: false,
+  },
+  {
+    id: 7,
+    url: "",
+    checked: false,
+  },
+  {
+    id: 8,
+    url: "",
+    checked: false,
+  },
+  {
+    id: 9,
+    url: "",
+    checked: false,
+  },
+];
 
 function page() {
   const [triggerNav, setTriggerNav] = useState<boolean>(false);
   const [mounted, setMounted] = useState(false);
   const [selectedFiles, setSelectedFiles] = useState<any[]>([]);
+  const [imageSelect, setImageSelect] = useState<any[]>([...imageList]);
+
+  const toggleSelectImage = (e: any, item: any) => {
+    console.log("eee", e, "item", item);
+    if (e.type === "click") {
+      setImageSelect(
+        imageSelect.map((d) =>
+          d.id === item.id ? { ...d, checked: !item.checked } : d
+        )
+      );
+    } else {
+      setImageSelect(
+        imageSelect.map((d) => {
+          return { ...d, checked: false };
+        })
+      );
+    }
+  };
 
   useEffect(() => {
     setMounted(true);
@@ -124,8 +193,8 @@ function page() {
                       <CustomImage
                         key={file && file?.name}
                         src={file?.preview}
-                        width={55}
-                        height={55}
+                        width={147}
+                        height={120}
                         // onLoad={() => {
                         //   URL.revokeObjectURL(selectedFiles[0]?.preview);
                         // }}
@@ -178,10 +247,59 @@ function page() {
           <Box
             sx={{
               w: "full",
-              minH: "20rem",
+              maxH: "20rem",
               overflowY: "scroll",
             }}
-          ></Box>
+          >
+            <SimpleGrid minChildWidth="250px" spacing="15px">
+              {imageSelect &&
+                imageSelect.length > 0 &&
+                imageSelect.map((image) => (
+                  <Box
+                    key={image.id}
+                    sx={{
+                      position: "relative",
+                    }}
+                    cursor={"pointer"}
+                    onClick={(e: any) => toggleSelectImage(e, image)}
+                  >
+                    {image.checked === false ? (
+                      <></>
+                    ) : (
+                      <Box
+                        position="absolute"
+                        right="30px"
+                        top="10px"
+                        zIndex={5}
+                      >
+                        <IoIosCheckbox
+                          fontSize={"2.5rem"}
+                          color={PandaCat.colorCheck}
+                        />
+                      </Box>
+                    )}
+
+                    <CustomImage
+                      src={image.url}
+                      width={250}
+                      height={80}
+                      alt="images"
+                    />
+                  </Box>
+                ))}
+            </SimpleGrid>
+          </Box>
+          <Divider mb="1.5rem" />
+          <Flex justify="flex-end">
+            <CustomButton
+              sx={{
+                maxW: "13rem",
+                alignSelf: "right",
+              }}
+            >
+              Generate meme
+            </CustomButton>
+          </Flex>
         </Box>
       </Box>
     </chakra.div>
